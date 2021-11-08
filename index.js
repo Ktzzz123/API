25d17bec4bd656d2a805483ff2b8504d5744881b
TieuThiKieuTien/18521491 <18521491@gm.uit.edu.vn>
2021-11-09 03:51:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses=[
+    {
+        id:1, name:'java1'
+    },
+    {
+        id:2, name:'java2',
+    },
+    {
+        id:3, name:'java3',
+    },
+    {
+        id:4, name:'java4',
+    },
+    {id:5, name:'java5',}
+]
+app.get('/', (req,res)=>{
+    res.send('Hello wfdfdforld');
+});
+
+app.get('/api/courses', (req,res) => {
+    res.send(courses);
+});
+
+app.post('/api/courses',(req,res)=>{
+    if(req.body.name.length<3){
+        res.status(400).send("invalid name");
+        return;
+    }
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('not found');
+    res.send(course);
+});
+
+
+const port = process.env.PORT||3000;
+app.listen(port, ()=>console.log(`Listening on port ${port}...`));
